6905b068e1c364ef47847e6aaf57b5dc44da9902
Orit Kozolin <orit@nachum.co.il>
2020-03-09 22:03:11 +0200

    parse and sort ui text in box

--- src/App.js
+++ src/App.js
@@ -4,49 +4,50 @@ import './App.css';
 export const ItemsContext = React.createContext();
 
 function App() {
-    const [uiGridText, setUiGridText] = useState([])
+    let elements = [];
 
-  const uiGrid = [
-    '2;1;gender;SELECT;Male,Female',
-    '1;1;First Name;TEXT_INPUT;Enter your first name',
-    '2;2;marital status;SELECT;Single,MAried,Divorced',
-    '1;2;Last Name;TEXT_INPUT;Enter your last name'
-  ]
-
-  const uiGrid2 = `
+    const uiGrid = `
 2;1;gender;SELECT;Male,Female
 1;1;First Name;TEXT_INPUT;Enter your first name
 2;2;marital status;SELECT;Single,MAried,Divorced
 1;2;Last Name;TEXT_INPUT;Enter your last name
   `
+    const [uiGridText, setUiGridText] = useState(uiGrid)
 
-  let elements = []
-    const parseElements = () => {
-    for (const elm of uiGrid ) {
-      elements.push(elm.split(';'))
+    const parseElements = (uiGrid) => {
+        //remove any white spaces at the start and split to rows
+        const elemArr = uiGrid.trim().split('\n')
+        for (const elm of elemArr ) {
+          elements.push(elm.split(';'))
+        }
+        // sort the array by row (first element) in ascending order
+        elements.sort(function(a, b) {
+          return a[0] - b[0];
+        });
+        console.log("elements", elements)
     }
 
-    const sortedArray = elements.sort(function(a, b) {
-      return a[0] - b[0];
-    });
-    console.log("elements", elements)
-  }
-  parseElements()
-
-  // Text Area element component
+    // Elements Components
+    // ---------------------------------
+    //
+    // Text Area element component
   const TextAreaElem = (props => {
-      const {label, value} = props
+      const {label} = props
       // used to pass state from child to parent  - pass UI text instructions
       const [items, setItems] = useContext(ItemsContext)
       const [gridText, setGridText] = useState('')
+
       const handleChange = (e) => {
           setGridText(e.target.value)
       }
+
       const handleGridTextChange = (e) => {
           console.log("I was cliked")
-          setItems([...items, gridText])
-          setGridText('')
-          console.log("items, gridText", items, gridText)
+          console.log("items", items)
+          console.log("gridText",gridText)
+          setItems(gridText)
+          debugger
+          // setGridText('')
       }
       return (
           <div>
@@ -54,7 +55,7 @@ function App() {
               <textarea id="ftext"
                         rows="8"
                         cols="50"
-                        defaultValue={ value }
+                        defaultValue={ items }
                         onChange={ handleChange }
               />
               <button onClick={ handleGridTextChange }>Generate Elements</button>
@@ -62,8 +63,6 @@ function App() {
       )
   })
 
-  // Elements Components
-  // ---------------------------------
   // Input element component
   const InputElm = (props) => {
     const {label, value} = props
@@ -99,9 +98,10 @@ function App() {
         Hello Orit
           <div id="grid_text_container">
               <ItemsContext.Provider value={[uiGridText, setUiGridText]}>
-                  <TextAreaElem value={uiGrid2}/>
+                  <TextAreaElem/>
               </ItemsContext.Provider>
           </div>
+          {parseElements(uiGridText)}
           <div id="elements_container">
               <InputElm label='first name' value='Enter your first name'/>
               <SelectElm label='Gender' value='Male,Female'/>
